7d3cf389c7abc0c6498949972dd9107e8115b0e9
immat0x1 #BLM #MIUI_GAY_PRIDE <53930905+popuguy4ik@users.noreply.github.com>
2020-08-16 11:01:19 +0500

    script cleanup and bug fix

--- common/install.sh
+++ common/install.sh
@@ -1,5 +1,4 @@
-E='ui_print " This module doesn't support ShitUI!" && abort " Aborting..."
-fi'
+OTVAL=[ui_print " This module doesn't support ShitUI!" && abort " Aborting..."]
 SPO="rm -rf $MODPATH/system/product/overlay"
 SVO="rm -rf $MODPATH/system/vendor/overlay"
 NAME=$(getprop ro.miui.ui.version.name)
@@ -13,13 +12,16 @@ ui_print " Device: $MODEL " && ui_print " Codename: $CODENAME " && ui_print " An
 ui_print " "
 
 if [ $NAME ]; then
-$E
+$OTVAL
+fi
 
 if [ $CODE ]; then
-$E
+$OTVAL
+fi
 
 if [ $REGION ]; then
-$E
+$OTVAL
+fi
 
 if [ ! $API -ge "29" ]; then
 abort "This Module for Android Q and R only!"
@@ -29,7 +31,14 @@ unzip -oj "$ZIPFILE" 'flag_value.xml' -d '/sdcard'
 ui_print " "&& ui_print "   Making ur NavBar looks better. "
 rm -rf /data/data/com.google.android.inputmethod.latin/shared_prefs/flag_value.xml
 cp '/sdcard/flag_value.xml' '/data/data/com.google.android.inputmethod.latin/shared_prefs/flag_value.xml'
+rm -rf /sdcard/flag_value.xml
 chmod 0644 /data/data/com.google.android.inputmethod.latin/shared_prefs/flag_value.xml
 am force-stop com.google.android.inputmethod.latin
-ui_print " " && ui_print "  And don't forget to sub to " && ui_print "  my channel in telegram : " && ui_print "  @oh_no_cr1nge " && ui_print "  it give me motivation " && ui_print "  to make more modules in future. " && ui_print " "
+ui_print " "
+ui_print "  And don't forget to sub to "
+ui_print "  my channel in telegram : "
+ui_print "  @oh_no_cr1nge "
+ui_print "  it give me motivation "
+ui_print "  to make more modules in future. "
+ui_print " "
 
